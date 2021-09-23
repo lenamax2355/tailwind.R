ca76253539433c0d04ebf8a2af2bc5ee3f469d26
TJ Palanca <mail@tjpalanca.com>
2021-09-23 12:34:54 +0000

    Add hex sticker

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# Tailwind CSS for R
+# Tailwind CSS for R <img src='man/figures/logo.png' align="right" height="139" />
 
 <!-- badges: start -->
 
---
+++ scripts/hex_sticker.R
@@ -0,0 +1,21 @@
+library(hexSticker)
+library(usethis)
+
+logo_location <- "inst/img/logo.png"
+
+sticker(
+  subplot =
+    grid::rasterGrob(png::readPNG("inst/img/tailwind.png"), interpolate = TRUE),
+  package  = "tailwind.R",
+  filename = logo_location,
+  p_size   = 20,
+  s_width  = 10,
+  s_height = 0.75,
+  s_x      = 1,
+  p_color  = "#16BECB",
+  h_color  = "#16BECB",
+  h_fill   = "#161D2D"
+)
+
+file.remove("man/figures/logo.png")
+use_logo(logo_location)
